fd1d64efde58697241d94901eb083c84ca88be6c
Matthew Wigginton Bhagat-Conway <matt@indicatrix.org>
2022-03-21 13:16:05 -0400

    fix include error

--- src/OSMPBF.jl
+++ src/OSMPBF.jl
@@ -5,7 +5,6 @@ include("proto/OSMPBF_pb.jl")
 include("model.jl")
 include("scan.jl")
 include("load.jl")
-include("osmtool/osmtool.jl")
 
 export scan_pbf, load_pbf, Node, Way, Relation, RelationMemberType
 end
